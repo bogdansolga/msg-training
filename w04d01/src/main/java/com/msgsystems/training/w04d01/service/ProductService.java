0fdbe36967f01485ce2c3f34bbec8fcbc1dc0fa0
Bogdan Solga <bogdan.solga@gmail.com>
2016-10-10 12:24:53 +0300

    [dev] Added an initial exception handling infrastructure

--- w04d01/src/main/java/com/msgsystems/training/w04d01/service/ProductService.java
+++ w04d01/src/main/java/com/msgsystems/training/w04d01/service/ProductService.java
@@ -2,6 +2,7 @@ package com.msgsystems.training.w04d01.service;
 
 import com.msgsystems.training.w04d01.data.model.Product;
 import com.msgsystems.training.w04d01.data.repository.ProductRepository;
+import com.msgsystems.training.w04d01.service.exceptions.ProductNotFoundException;
 
 @SuppressWarnings("unused")
 public class ProductService {
@@ -12,19 +13,38 @@ public class ProductService {
         this.productRepository = productRepository;
     }
 
-    public void saveProduct(final Product product) {
+    /** CRUD operations */
+
+    public void saveProduct(final Product product) throws Exception {
+        // perform very complex logic before saving the product...
         productRepository.saveProduct(product);
     }
 
-    public Product getProduct(final int id) {
-        return productRepository.getProduct(id);
+    public Product readProduct(final int id) throws Exception {
+        if (id <= 0) {
+            throw new IllegalArgumentException("Please provide a positive ID");
+        }
+
+        Product byId = productRepository.getProduct(id);
+        // do complex processing on the obtained product, before returning it to the controller
+
+        /* if (Java8)
+        return Optional.ofNullable(byId)
+                       .orElseThrow(() -> new ProductNotFoundException("No product with the id " + id));
+        */
+
+        if (byId != null) {
+            return byId;
+        } else {
+            throw new ProductNotFoundException("There is no product with the id " + id);
+        }
     }
 
-    public void updateProduct(final Product product) {
+    public void updateProduct(final Product product) throws Exception {
         productRepository.updateProduct(product);
     }
 
-    public void deleteProduct(final Product product) {
+    public void deleteProduct(final Product product) throws Exception {
         productRepository.deleteProduct(product);
     }
 }
