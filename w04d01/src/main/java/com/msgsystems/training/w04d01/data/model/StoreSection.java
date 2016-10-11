fa9ed7c005387beeabbefeddca32bcd1ed54cc2c
Bogdan Solga <bogdan.solga@gmail.com>
2016-10-11 11:32:45 +0300

    [dev] Added the identity generation

--- w04d01/src/main/java/com/msgsystems/training/w04d01/data/model/StoreSection.java
+++ w04d01/src/main/java/com/msgsystems/training/w04d01/data/model/StoreSection.java
@@ -4,6 +4,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -19,6 +21,7 @@ import java.util.Set;
 public class StoreSection implements Serializable {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name", length = 40)
