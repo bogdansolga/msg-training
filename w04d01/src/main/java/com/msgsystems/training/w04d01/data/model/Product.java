90c655a6b539997a7f6de4f74f44d9fa15d6a6f5
Bogdan Solga <bogdan.solga@gmail.com>
2016-10-11 11:30:26 +0300

    [dev] Added a named query

--- w04d01/src/main/java/com/msgsystems/training/w04d01/data/model/Product.java
+++ w04d01/src/main/java/com/msgsystems/training/w04d01/data/model/Product.java
@@ -1,5 +1,6 @@
 package com.msgsystems.training.w04d01.data.model;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
@@ -16,6 +18,12 @@ import java.util.Objects;
 @Table(
         name = "Product",
         schema = "msg_training"
+)
+@NamedQuery(
+        name = "Product.bySection",
+        query = "SELECT product " +
+                "FROM Product product " +
+                "WHERE product.storeSection.store.id = :storeId"
 )
 public class Product implements Serializable {
 
@@ -27,7 +35,7 @@ public class Product implements Serializable {
     @Column(name = "name", unique = true, nullable = false, insertable = true, updatable = false, length = 50)
     private String name;
 
-    @ManyToOne(targetEntity = StoreSection.class, fetch = FetchType.LAZY)
+    @ManyToOne(targetEntity = StoreSection.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "sectionId")
     private StoreSection storeSection;
 
